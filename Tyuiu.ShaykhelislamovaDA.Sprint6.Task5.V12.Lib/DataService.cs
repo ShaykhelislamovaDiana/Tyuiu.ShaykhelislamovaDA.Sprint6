5834cd406ba523485a246444a34a0b8ba68459f3
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-12-06 19:38:09 +0500

    Task5

--- Tyuiu.ShaykhelislamovaDA.Sprint6.Task5.V12.Lib/DataService.cs
+++ Tyuiu.ShaykhelislamovaDA.Sprint6.Task5.V12.Lib/DataService.cs
@@ -6,40 +6,28 @@ namespace Tyuiu.ShaykhelislamovaDA.Sprint6.Task5.V12.Lib
         public int len = 0;
         public double[] LoadFromDataFile(string path)
         {
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null) { len++; }
-            }
 
-            
 
-            int index = 0;
-            string[] dig0;
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    dig0 = line.Split(' ');
-                    len = dig0.Length;
+                    len++;
                 }
             }
 
             double[] numsArray = new double[len];
 
+            int index = 0;
+
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    line = line.Replace('.', ',');
-                    string[] dig = line.Split(' ');
-                    for (int i = 0; i < len; i++)
-                    {
-                        numsArray[index] = Convert.ToDouble(dig[index]);
-                        index++;
-                    }
+                    numsArray[index] = Convert.ToDouble(line);
+                    index++;
                 }
             }
 
